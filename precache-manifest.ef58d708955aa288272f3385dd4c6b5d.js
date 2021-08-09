f269aa1376a962d08b704c815aa3654bbe41232f
rohannaam221b <your_email>
2021-08-09 08:48:03 +0530

    Updates

---
+++ precache-manifest.ef58d708955aa288272f3385dd4c6b5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ca865fc1f2477fd3bb1c63e8f26ff7d",
+    "url": "/smart-Brain/index.html"
+  },
+  {
+    "revision": "0ec2e8e8a3b8685bcfb3",
+    "url": "/smart-Brain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "565fdce5d6536ceb5331",
+    "url": "/smart-Brain/static/css/main.ffead349.chunk.css"
+  },
+  {
+    "revision": "0ec2e8e8a3b8685bcfb3",
+    "url": "/smart-Brain/static/js/2.d439fa25.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/smart-Brain/static/js/2.d439fa25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "565fdce5d6536ceb5331",
+    "url": "/smart-Brain/static/js/main.fa378e6b.chunk.js"
+  },
+  {
+    "revision": "d2d874589059a9400d6f",
+    "url": "/smart-Brain/static/js/runtime-main.a01805f5.js"
+  }
+]);
